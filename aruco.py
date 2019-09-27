a589743aa5a87edbc0157fcadf95af799834b5fe
Emma Smith <saoirse-zee@users.noreply.github.com>
2019-09-27 14:23:19 -0400

    Detect arucos in live stream

--- aruco.py
+++ aruco.py
@@ -4,21 +4,18 @@ import cv2.aruco as aruco
 
 def do_arcuo_thing(cap):
   frame = cap.read()[1]
+  scale_percent = 100
+  width = int(frame.shape[1] * scale_percent / 100)
+  height = int(frame.shape[0] * scale_percent / 100)
+  dimemsions = (width, height)
+  frame = cv2.resize(frame, dimemsions, interpolation = cv2.INTER_AREA)
+  markers = aruco.detectMarkers(frame, aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL))
+  frame = aruco.drawDetectedMarkers(frame, markers[0], markers[1])
   cv2.imshow("aruco test", frame)
-  # img = cv2.imread("aruco-test-image-3-7.jpg", cv2.IMREAD_UNCHANGED)
 
-  # scale_percent = 100
-  # width = int(img.shape[1] * scale_percent / 100)
-  # height = int(img.shape[0] * scale_percent / 100)
-  # dim = (width, height)
-  # img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-
-  # markers = aruco.detectMarkers(img, aruco.Dictionary_get(aruco.DICT_4X4_50))
-  # img = aruco.drawDetectedMarkers(img, markers[0], markers[1])
-
-  # cv2.imshow("", img)
+  # cv2.imshow("", frame)
   # cv2.waitKey()
-  
+
   if cv2.waitKey(1) & 0xFF == ord('q'):
     exit()
   
