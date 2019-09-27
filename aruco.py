62bb7f92601c42d86a7ec704296df23b4aef2604
Emma Smith <saoirse-zee@users.noreply.github.com>
2019-09-27 15:06:22 -0400

    Fix misspelling

--- aruco.py
+++ aruco.py
@@ -1,27 +1,29 @@
 import cv2
 import cv2.aruco as aruco
 
-
-def do_arcuo_thing(cap):
+def do_aruco_thing(cap):
   frame = cap.read()[1]
+
+  # Resize the frame
   scale_percent = 100
   width = int(frame.shape[1] * scale_percent / 100)
   height = int(frame.shape[0] * scale_percent / 100)
   dimemsions = (width, height)
   frame = cv2.resize(frame, dimemsions, interpolation = cv2.INTER_AREA)
+
+  # Find markers and draw them
   markers = aruco.detectMarkers(frame, aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL))
   frame = aruco.drawDetectedMarkers(frame, markers[0], markers[1])
+  
+  # Show it
   cv2.imshow("aruco test", frame)
-
-  # cv2.imshow("", frame)
-  # cv2.waitKey()
-
+  
+  # Escape hatch
   if cv2.waitKey(1) & 0xFF == ord('q'):
     exit()
-  
 
 if __name__ == "__main__":
   cap = cv2.VideoCapture(0)
   cv2.namedWindow("aruco test")
   while True:
-    do_arcuo_thing(cap)
+    do_aruco_thing(cap)
