dc3e93cff58cc33c915155799d96cf2f8784b294
Emma Smith <saoirse-zee@users.noreply.github.com>
2019-10-02 12:07:12 -0400

    Manually put dots in corners

--- aruco.py
+++ aruco.py
@@ -2,62 +2,54 @@ import cv2
 import cv2.aruco as aruco
 import numpy as np
 
+# Config - dependent on projector and table, etc
+X_FACTOR_TOP_LEFT = 0.087890625 
+Y_FACTOR_TOP_LEFT = 0.078125
+X_FACTOR_BOTTOM_LEFT = 0.8
+Y_FACTOR_BOTTOM_LEFT = 0.6
+
 def corner_points_to_point(corner):
   return corner[0][0]
 
 def do_aruco_thing(cap):
-  corner_ids = [393, 19, 193, 979]
   # Use camera 1, which is the attached usb camera.
   frame = cap.read()[1]
 
   # Resize the frame
-  scale_percent = 100
+  scale_percent = 50
   width = int(frame.shape[1] * scale_percent / 100)
   height = int(frame.shape[0] * scale_percent / 100)
   dimensions = (width, height)
   frame = cv2.resize(frame, dimensions, interpolation = cv2.INTER_AREA)
 
-  # Find markers and draw them
-  aruco_markers = aruco.detectMarkers(frame, aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL))
-  frame = cv2.rectangle(frame, (0,0), dimensions, (0,0,0), cv2.FILLED)
-  corners = aruco_markers[0]
-  ids = aruco_markers[1]
-  if ids is not None:
-    unique_ids = { id[0] : indx for indx, id in enumerate(ids) }
-  else:
-    unique_ids = {}
+  def draw_corner(frame, x_factor, y_factor):
+    corner_position = (int(x_factor * width), int(y_factor * height))
+    frame = cv2.circle(frame, corner_position, 10, (200, 100, 100), 5)
+    return frame
 
-  # if 393 in unique_ids and 19 in unique_ids and 193 in unique_ids and 979 in unique_ids:
-  if all([id in unique_ids for id in corner_ids]):
-    yfacty = .7
-    xfacty = 1
-    src = np.array(
-      [
-        corner_points_to_point(corners[unique_ids[393]]),
-        corner_points_to_point(corners[unique_ids[19]]),
-        corner_points_to_point(corners[unique_ids[193]]),
-        corner_points_to_point(corners[unique_ids[979]])
-      ]
-    )
-    dst = np.array(
-      [
-        [0, 0],
-        [width * xfacty, 0],
-        [0, height * yfacty],
-        [width * xfacty, height * yfacty]
-      ]
-    )
-    do_aruco_thing.homography = cv2.findHomography(src, dst)[0]
+  frame = draw_corner(frame, X_FACTOR_TOP_LEFT, Y_FACTOR_TOP_LEFT)
+  frame = draw_corner(frame, X_FACTOR_BOTTOM_LEFT, Y_FACTOR_BOTTOM_LEFT)
+  
+  
 
-  for idx, id in enumerate(ids):
-    if id not in corner_ids:
-      pt = corner_points_to_point(corners[idx])
-      pt = (pt[0], pt[1], 1)
-      pt = np.dot(do_aruco_thing.homography, pt)
-      pt = (int(pt[0]), int(pt[1]))
-      frame = cv2.circle(frame, pt, 40, (0, 0, 255))
-
-  frame = aruco.drawDetectedMarkers(frame, corners, ids)
+  # Find homography
+  # src = np.array(
+  #   [
+  #     corner_points_to_point(some_corner),
+  #     corner_points_to_point(some_corner),
+  #     corner_points_to_point(some_corner),
+  #     corner_points_to_point(some_corner),
+  #   ]
+  # )
+  # dst = np.array(
+  #   [
+  #     corner_points_to_point(some_corner),
+  #     corner_points_to_point(some_corner),
+  #     corner_points_to_point(some_corner),
+  #     corner_points_to_point(some_corner),
+  #   ]
+  # )
+  # do_aruco_thing.homography = cv2.findHomography(src, dst)[0]
 
   # Show it
   cv2.imshow("aruco test", frame)
@@ -67,9 +59,14 @@ def do_aruco_thing(cap):
     exit()
 
 if __name__ == "__main__":
+  # Hacky thing to find the right camera, ugh
   for i in range(5):
     cap = cv2.VideoCapture(1)
+  
+  # Init the homography matrix to the "identity matrix"
   do_aruco_thing.homography = np.identity
+  
   cv2.namedWindow("aruco test")
+  
   while True:
     do_aruco_thing(cap)
