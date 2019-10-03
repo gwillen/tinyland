2a5167ce9185b97866891edeb58a4b16b972cfc6
Emma Smith <saoirse-zee@users.noreply.github.com>
2019-10-03 14:20:14 -0400

    Find Aruco markers and (badly) draw them.

--- calibrate.py
+++ calibrate.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import cv2.aruco as aruco
 
 # Config - Dependent on projector and camera position, so fiddle with these until they line up with the corners of your monitor
 PROJECTOR_WIDTH = 1366
@@ -22,36 +23,50 @@ DEST_CORNERS = np.array([
 
 VERTICAL_OFFSET = -45 # For title bar
 
+def correctImage(original_image, homography, dimensions):
+    correctedImage = cv2.warpPerspective(original_image, homography, (PROJECTOR_WIDTH, PROJECTOR_HEIGHT)) # TODO: magic nums, use config constants
+    correctedImage = cv2.flip(correctedImage, -1)
+    T = np.float32([[1, 0, 0], [0, 1, VERTICAL_OFFSET]])
+    correctedImage = cv2.warpAffine(correctedImage, T, dimensions) 
+    return correctedImage
+
 def calibrate(cap):
   # Use camera 1, which is the attached usb camera.
   frame = cap.read()[1]
-
-  # Resize the frame
   scale_percent = 100
   width = int(frame.shape[1] * scale_percent / 100)
   height = int(frame.shape[0] * scale_percent / 100)
   dimensions = (width, height)
-  frame = cv2.resize(frame, dimensions, interpolation = cv2.INTER_AREA)
+
+  # Resize the frame
+  frame = cv2.resize(frame, dimensions, interpolation = cv2.INTER_AREA) # QUESTION: Does this need to happen here? Move to `correctImage`?
 
   # Escape hatch
   if cv2.waitKey(1) & 0xFF == ord('q'):
     exit()
   
-  # Homography stuff
-  homography, status = cv2.findHomography(SRC_CORNERS, DEST_CORNERS)
-  correctedImage = cv2.warpPerspective(frame, homography, (PROJECTOR_WIDTH, PROJECTOR_HEIGHT)) # TODO: magic nums, use config constants
-  correctedImage = cv2.flip(correctedImage, -1)
+  # Homography
+  h, status = cv2.findHomography(SRC_CORNERS, DEST_CORNERS)
 
-  T = np.float32([[1, 0, 0], [0, 1, VERTICAL_OFFSET]])
-  correctedImage = cv2.warpAffine(correctedImage, T, (width, height)) 
+  # Correction
+  image = correctImage(frame, h, dimensions)
+
+  # Aruco - Find markers
+  aruco_markers = aruco.detectMarkers(frame, aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL))
+  corners = aruco_markers[0]
+  ids = aruco_markers[1]
+
+  # Render
   BLACK = (0, 0, 0)
   WHITE = (255, 255, 255)
-  correctedImage = cv2.rectangle(correctedImage, (0,0), (PROJECTOR_WIDTH, PROJECTOR_HEIGHT), BLACK, cv2.FILLED)
-  correctedImage = cv2.circle(correctedImage, (100, 100), 30, WHITE, 5)
+  image = cv2.rectangle(image, (0,0), (PROJECTOR_WIDTH, PROJECTOR_HEIGHT), BLACK, cv2.FILLED)
+  frame = aruco.drawDetectedMarkers(frame, corners, ids)
+  image = aruco.drawDetectedMarkers(image, corners, ids)
+  # image = cv2.circle(image, (100, 100), 30, WHITE, 5)
   
   # Show it
   cv2.imshow("Tinycam", frame)
-  cv2.imshow("Tinyland", correctedImage)
+  cv2.imshow("Tinyland", image)
 
 def printXY(_a, x, y, _b, _c):
   print("x: ", x)
