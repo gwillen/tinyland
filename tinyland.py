c00e2199e00f9058fbbd448261c2c58c729191f6
Emma Smith <saoirse-zee@users.noreply.github.com>
2019-10-09 19:32:45 -0400

    Make rects in demo larger
    The small rects were causing cv2 to freak itself out. The marker detection was flickering on and off, I think because the edge of the projected rect crossed the edge of the aruco marker.

--- tinyland.py
+++ tinyland.py
@@ -209,8 +209,8 @@ class Landscape:
     # Draw shapes to context based on data in snapshot
     for id, markers in snap.markers.items():
       for marker in markers:
-        ctx.rect(marker.center.x, marker.center.y, 20, 20)
-        ctx.text(marker.center.x, marker.center.y, str(marker.id))
+        ctx.rect(marker.center.x, marker.center.y, 150, 150)
+        ctx.text(marker.center.x, marker.center.y + 160, str(marker.id))
 
     # Render projection
     self.project(ctx)
